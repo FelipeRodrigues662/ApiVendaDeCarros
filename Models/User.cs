f1350ab9e00e46935dfbdd88c53accb05eed8ba7
FelipeRodrigues662 <Felipe.Rod00@hotmail.com>
2022-12-23 13:56:08 -0300

    User

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiSqlAsp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserNames { get; set; }
+        public string Password { get; set; }
+        public string Autorizacao { get; set; }
+    }
+}
