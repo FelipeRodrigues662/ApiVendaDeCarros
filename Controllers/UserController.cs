63435f7dd0fb31cb4adc15c08b70b484686743fd
FelipeRodrigues662 <Felipe.Rod00@hotmail.com>
2022-12-26 10:40:33 -0300

    AddUser

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ApiSqlAsp.Models;
+using ApiSqlAsp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSqlAsp.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class UserController : ControllerBase
+    {
+        private readonly User user;
+
+        [HttpPost("login")]
+        public IActionResult Login([FromServices] TokenService tokenService)
+        {
+            var token = tokenService.GenerateToken(user);
+
+            return Ok(token);
+        }
+    }
+}
--- Migrations/20221226133708_UpdateUser.Designer.cs
+++ Migrations/20221226133708_UpdateUser.Designer.cs
@@ -3,6 +3,7 @@ using ApiSqlAsp.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiSqlAsp.Migrations
 {
     [DbContext(typeof(ApiDataContext))]
-    partial class ApiDataContextModelSnapshot : ModelSnapshot
+    [Migration("20221226133708_UpdateUser")]
+    partial class UpdateUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
