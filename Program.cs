52e4dcc93d683722ee489c1759db4558001cc922
Felipe Pereira Rodrigues <82169870+FelipeRodrigues662@users.noreply.github.com>
2022-12-27 13:21:19 -0300

    Revert "UserController"

--- Program.cs
+++ Program.cs
@@ -1,27 +1,7 @@
-using ApiSqlAsp;
 using ApiSqlAsp.DataContext;
 using ApiSqlAsp.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-
-builder.Services.AddAuthentication(configureOptions: x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApiDataContext>();
@@ -31,11 +11,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.MapControllers();
 app.Run();
